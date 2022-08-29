1d365e7c8ae3b0ea0262faee61ea5084cba0890e
Filipe Machado <filipe__machado@hotmail.com>
2022-08-29 02:45:22 -0300

    feat: add basic structure to the mud game

--- app/game.py
+++ app/game.py
@@ -1,1 +1,58 @@
+from database import DatabaseConnection
 
+
+class Game:
+    
+    def __init__(self):
+        db_connection = DatabaseConnection()
+        pass
+
+    def start(self):
+        print('Jogo Iniciou')
+        self.menu()
+        
+    def menu(self):
+        self.display_menu()
+        player_choice = input('Escolha uma opção')
+
+    def start_new_game(self):
+        print('Novo Jogo!')
+        pass
+    
+    def load_game(self):
+        print('Carregar Jogo!')
+        pass
+    
+    def exit(self):
+        pass
+        
+    def display_menu(self):
+        menu = {
+            '1': {
+                'str':'Iniciar novo jogo',
+                'func': self.start_new_game
+            },
+            '2': {
+                'str':'Carregar jogo',
+                'func': self.load_game},
+            '0': {
+                'str': 'Sair',
+                'func':self.exit
+            }            
+        }
+        for item in menu:
+            print(f'{item}) {menu.get(item).get("str")}')
+        player_choice = input('Escolha uma opção: ')
+        
+        if player_choice:
+            menu.get(str(player_choice)).get('func')()
+        else:
+            exit()
+    
+    def read_menu_input(self):
+        pass
+
+        
+if __name__ == '__main__':
+    game = Game()
+    game.start()

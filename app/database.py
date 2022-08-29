095c14f111e74d86b48fac7628149bda3cd98591
Filipe Machado <filipe__machado@hotmail.com>
2022-08-29 03:36:39 -0300

    feat: add basic structure for command

--- app/database.py
+++ app/database.py
@@ -1,7 +1,18 @@
+import psycopg2
+
+
 
 class DatabaseConnection:
     
     def __init__(self):
+        # TODO Change this later
+        connection = psycopg2.connect(
+            dbname='postgres',
+            user='postgres',
+            password='postgres',
+            host='localhost'
+        )
+        print(connection)
         pass
 
 

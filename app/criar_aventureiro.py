9f94e16036ee445d79b99dc6220b93c9ae7c4fe6
PedroLucas <siqueirafernandes02@gmail.com>
2022-08-29 17:39:47 -0300

    Mais alteraçoes

--- app/criar_aventureiro.py
+++ app/criar_aventureiro.py
@@ -1,12 +1,12 @@
 import psycopg2
 from app.conectar import conecta_db
 
-def inserir_aventureiro(_nome, _raca, _classe, _regiao):
+def inserir_aventureiro(_nome, _raca, _classe, _regiao, _inst_cabeca, _inst_tronco, _inst_LHand, _inst_RHand):
     con = conecta_db()
     cur = con.cursor()
     try:
         #tem que ver como vai entrar o nome, raca, classe e regiao
-        cur.execute(f'call insert_aventureiro(\'{_nome}\', \'{_raca}\', \'{_classe}\', \'{_regiao}\')')
+        cur.execute(f'call insert_aventureiro(\'{_nome}\', \'{_raca}\', \'{_classe}\', \'{_regiao}\', \'{_inst_cabeca}\',  \'{_inst_tronco}\',  \'{_inst_LHand}\',  \'{_inst_RHand}\')')
         con.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error: %s" % error)

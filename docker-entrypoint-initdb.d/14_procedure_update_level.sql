3a3c40620fc31ce631e32a29decf3439acbe12cb
Ian Rocha <ianrocha.df@gmail.com>
2022-08-29 22:51:32 -0300

    Correção

--- docker-entrypoint-initdb.d/14_procedure_update_level.sql
+++ docker-entrypoint-initdb.d/14_procedure_update_level.sql
@@ -2,7 +2,7 @@
 create or replace function upar_nivel() as $upar_nivel$
     begin
         if(old.xp <> new.xp) then
-            if(new.xp = ) --ta faltando um limitador pra saber quando upar
+            if(new.xp = 100) 
             update aventureiro set nivel = nivel + 1;
         end if;
         return null;

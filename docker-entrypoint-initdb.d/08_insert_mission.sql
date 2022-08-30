7632e787a6cbc2ae61aab4b73397c87b843314a8
Filipe Machado <filipe__machado@hotmail.com>
2022-08-29 21:14:47 -0300

    fix: fix docker entrypoint init

--- docker-entrypoint-initdb.d/08_insert_mission.sql
+++ docker-entrypoint-initdb.d/08_insert_mission.sql
@@ -1,15 +1,15 @@
 -- POPULAR MISSÕES
-INSERT INTO missao(descricao)
-VALUES ('Registro na Guilda de Aventureiros');
-INSERT INTO missao(descricao)
-VALUES ('Derrotar um Morto-vivo');
-
--- POPULAR REQUISITOS DE MISSÕES
-INSERT INTO requisitos_missao(id_missao, descricao)
-VALUES (2, 'Estar registrado na Guilda de Aventureiros');
-
--- POPULAR OBJETIVOS DE MISSÕES
-INSERT INTO objetivo_missao(id_missao, descricao)
-VALUES (1, 'Se registrar como Aventureiro na Guilda');
-INSERT INTO objetivo_missao(id_objetivo_missao, id_missao, descricao)
-VALUES (2, 'Matar 1 Morto-vivo');
+-- INSERT INTO missao(descricao)
+-- VALUES ('Registro na Guilda de Aventureiros');
+-- INSERT INTO missao(descricao)
+-- VALUES ('Derrotar um Morto-vivo');
+--
+-- -- POPULAR REQUISITOS DE MISSÕES
+-- INSERT INTO requisitos_missao(id_missao, descricao)
+-- VALUES (2, 'Estar registrado na Guilda de Aventureiros');
+--
+-- -- POPULAR OBJETIVOS DE MISSÕES
+-- INSERT INTO objetivo_missao(id_missao, descricao)
+-- VALUES (1, 'Se registrar como Aventureiro na Guilda');
+-- INSERT INTO objetivo_missao(id_objetivo_missao, id_missao, descricao)
+-- VALUES (2, 'Matar 1 Morto-vivo');

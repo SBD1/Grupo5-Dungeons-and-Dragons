426f8957dc8a814546647b0bd084daaaead6e900
PedroLucas <siqueirafernandes02@gmail.com>
2022-09-05 09:40:03 -0300

    Criacao missao/requisito/objetivo

--- docker-entrypoint-initdb.d/17_procedure_criar missao.sql
+++ docker-entrypoint-initdb.d/17_procedure_criar missao.sql
@@ -10,15 +10,30 @@ create or replace procedure create_requisito(_descricao_requisito text) as
 
 -- PROCEDURE CRIAR OBJETIVO
 create or replace procedure create_objetivo(_id_ missao int, _descricao_objetivo text) as 
-	$create_requisito$
+	$create_objetivo$
 		declare 
             _id_objetivo integer;
 		begin
             create into objetivo_missao(id_missao, descricao) values(_id_missao, _descricao_objetivo);
 		end;
-	$create_requisito$ language 'plpgsql';
+	$create_objetivo$ language 'plpgsql';
 
 -- PROCEDURE CRIAR MISSAO
+create or replace procedure create_missao(
+    _descricao text,
+    _descricao_objetivo text
+    ) as 
+	$create_missao$
+    declare 
+			_id integer;
+		begin
+            create into missao(descricao) values(_descricao) returning id_missao into _id;
+            call create_objetivo(_id, _descricao_objetivo);
+		end;
+	$create_missao$ language 'plpgsql';
+
+-- Verificar quais objetivos tem com um ID de missao X
+
 create or replace procedure create_missao(
     _descricao text,
     _descricao_objetivo text

2e53cf1c4e1f6fe38df97d960b0763434d4b739a
fmaachadoo <filipe__machado@hotmail.com>
2022-09-11 18:55:29 -0300

    feat: add more fields to location search

--- docker-entrypoint-initdb.d/05_0_procedure_get_location.sql
+++ docker-entrypoint-initdb.d/05_0_procedure_get_location.sql
@@ -4,9 +4,13 @@ create or replace procedure get_player_location(
     inout _regiao_nome varchar(50),
     inout _regiao_descricao text,
     inout _norte_nome varchar(50),
+    inout _norte_id integer,
     inout _sul_nome varchar(50),
+    inout _sul_id integer,
     inout _leste_nome varchar(50),
-    inout _oeste_nome varchar(50)
+    inout _leste_id integer,
+    inout _oeste_nome varchar(50),
+    inout _oeste_id integer
 ) as
 $get_player_location$
 begin
@@ -14,18 +18,22 @@ begin
         r.id_regiao,
         r.nome,
         r.descricao,
-        rn.nome,
-        rs.nome,
-        rl.nome,
-        ro.nome
+        rn.nome, rn.id_regiao,
+        rs.nome, rs.id_regiao,
+        rl.nome, rl.id_regiao,
+        ro.nome, ro.id_regiao
     into
         _regiao_id,
         _regiao_nome,
         _regiao_descricao,
         _norte_nome,
+        _norte_id,
         _sul_nome,
+        _sul_id,
         _leste_nome,
-        _oeste_nome
+        _leste_id,
+        _oeste_nome,
+        _oeste_id
     from regiao r
         join aventureiro a on r.id_regiao = a.regiao
         left outer join regiao rn on (rn.id_regiao = r.norte)
